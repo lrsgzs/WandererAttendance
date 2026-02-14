354834899ebaa864bba1a79f5a079f09f6c09927
lrs2187 <liurongshuo2022@outlook.com>
2026-02-14 20:37:45 +0800

    init: 空白模板

---
+++ WandererAttendance.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace WandererAttendance.Android;
+
+[Activity(
+    Label = "WandererAttendance.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont();
+    }
+}
